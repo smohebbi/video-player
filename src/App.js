652bd8f6f468aa47455859bccd98204bbf0f9795
smohebbi <siavash.mohebbi@gmail.com>
2022-11-19 16:05:19 +0100

    resolved warnings and errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Video } from "./Video";
+import { Menu } from "./Menu";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const VIDEOS = {
+  fast: "https://content.codecademy.com/courses/React/react_video-fast.mp4",
+  slow: "https://content.codecademy.com/courses/React/react_video-slow.mp4",
+  cute: "https://content.codecademy.com/courses/React/react_video-cute.mp4",
+  eek: "https://content.codecademy.com/courses/React/react_video-eek.mp4",
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { src: VIDEOS.fast };
+    this.chooseVideo = this.chooseVideo.bind(this);
+    this.handler = this.handler.bind(this);
+  }
+
+  handler(e) {
+    let type = e.target.value;
+    this.setState({src: VIDEOS[type]})
+  }
+  //
+  
+  chooseVideo(newVideo) {
+    this.setState({
+      src: VIDEOS[newVideo],
+    });
+  }
+  //chooseVideo={this.chooseVideo}
+  render() {
+    return (
+      <div>
+        <h1>Video Player</h1>
+        <Menu 
+          onClick={this.handler} />
+        <Video src={this.state.src} />
+      </div>
+    );
+  }
 }
 
 export default App;
